485ef1e9d3bdd4bd4efc1e1a8cbd26bec7b1b525
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-19 21:58:50 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
-def my_select(collection) 
-  empty_arr = [] 
-  
-  i=0 
-  while i < collection.length
-  empty_arr.push yield(collection[i]) 
-  i += 1 
-end 
-
-  empty_arr.compact 
-end 
+def my_select(collection)
+ # your code here!
+ new = []
+ i = 0
+ while i < collection.length
+    new << yield(collection[i])
+    i += 1
+  end
+    new
+end
